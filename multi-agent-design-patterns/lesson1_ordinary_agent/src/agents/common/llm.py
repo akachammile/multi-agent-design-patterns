55e21e1d6cb70681ed388d3f380a71094bc9c02d
akachammile <1224307033@qq.com>
2026-02-02 18:03:12 +0800

    feat: 添加LLM模型加载器和配置模块入口。

---
+++ multi-agent-design-patterns/lesson1_ordinary_agent/src/agents/common/llm.py
@@ -0,0 +1,23 @@
+import os
+from dotenv import load_dotenv
+from src.configs import config
+from langchain.chat_models import init_chat_model, BaseChatModel
+
+
+def load_specific_model(full_model_name: str, **kwargs) -> BaseChatModel:
+
+    provider, model = full_model_name.split("/", maxsplit=1)
+    model_info = config.model_names.get(provider)
+    if not model_info:
+        raise ValueError(f"Unknown model provider: {provider}")
+    api_key = os.getenv(model_info.env, [])
+
+    if provider in ["openai", "deepseek"]:
+        model_spec = f"{provider}:{model}"
+        logger.debug(f"[offical] Loading model {model_spec} with kwargs {kwargs}")
+        return init_chat_model(model_spec, **kwargs)
+
+    elif provider in ["dashscope"]:
+        from langchain_openai import ChatOpenAI
+
+        return ChatOpenAI()

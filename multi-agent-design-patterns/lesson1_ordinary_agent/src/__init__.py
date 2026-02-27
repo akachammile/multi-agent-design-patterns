c312064b5519964b399e28ffb4e5b5a013a21712
akachaosbox <1224307033@qq.com>
2026-02-27 22:25:06 +0800

    chore: Delete agent design pattern

--- multi-agent-design-patterns/lesson1_ordinary_agent/src/__init__.py
+++
@@ -1,4 +0,0 @@
-from dotenv import load_dotenv
-
-load_dotenv(".env", override=True)
-from src.configs import config
--- multi-agent-design-patterns/lesson1_ordinary_agent/src/database/postgres/base.py
+++
@@ -1,7 +0,0 @@
-"""
-SQLAlchemy 声明式基类 - 所有模型的共享基类
-"""
-
-from sqlalchemy.orm import declarative_base
-
-Base = declarative_base()

7ab9b30fca9362adf26505dfa2581b2630a3705b
akachaosbox <1224307033@qq.com>
2026-03-03 23:42:57 +0800

    feat: 创建base agent，需要集合上下文，所在路径，名称等后续会设计为configurable的方式

--- multi-agent-design-patterns/lesson1_ordinary_agent/src/agents/common/base_agent.py
+++ multi-agent-design-patterns/lesson1_ordinary_agent/src/agents/common/base_agent.py
@@ -1,8 +1,8 @@
+from abc import abstractmethod
 from .base_context import BaseContext
 
 
 class BaseAgent:
-
     name: str = "base_agent"
     description: str = "底层代理"
     context: type[BaseContext] = BaseContext
@@ -10,11 +10,23 @@ class BaseAgent:
     def __init__(self):
         pass
 
-    def stream_messages(
-        self,
-        input,
-    ):
+    @property
+    def module_name(self):
+        return self.__class__.__name__
+
+    def id(self):
+        return f"{self.module_name}_{self.name}"
+
+    @abstractmethod
+    def get_agent(self):
+        """
+        获取agent, 需子类重写
+        """
+        pass
+
+    def stream(self, messages, context=None, **kwargs):
         """
         流式输出后处理
         """
+
         pass

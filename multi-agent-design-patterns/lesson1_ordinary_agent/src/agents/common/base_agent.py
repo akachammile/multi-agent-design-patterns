7b830f3243ac2a312818f30e9f805c53a48bafdd
akachammile <1224307033@qq.com>
2026-03-03 17:53:22 +0800

    feat: 这B电脑我真受不了了，我草泥马

--- multi-agent-design-patterns/lesson1_ordinary_agent/src/agents/common/base_agent.py
+++ multi-agent-design-patterns/lesson1_ordinary_agent/src/agents/common/base_agent.py
@@ -1,12 +1,19 @@
+from .base_context import BaseContext
+
+
 class BaseAgent:
 
     name: str = "base_agent"
     description: str = "底层代理"
-    context: 
+    context: type[BaseContext] = BaseContext
+
     def __init__(self):
         pass
 
-    def stream_messages(self):
+    def stream_messages(
+        self,
+        input,
+    ):
         """
         流式输出后处理
         """
